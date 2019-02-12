9259d45c2d1aef3abde2138d7e2636458530cd36
mgrinko <glukym@gmail.com>
2019-02-12 13:28:38 +0200

    Simple state voter implementation

---
+++ js/main.js
@@ -0,0 +1,15 @@
+import Voter from './voter.js';
+
+new Voter({
+  element: document.querySelector('[data-component="voter1"]')
+});
+
+new Voter({
+  element: document.querySelector('[data-component="voter2"]')
+});
+
+new Voter({
+  element: document.querySelector('[data-component="voter3"]')
+});
+
+
