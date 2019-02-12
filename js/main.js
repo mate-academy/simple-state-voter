6b043f0df7e5b189b107cd62126ea51cbc7b501d
mgrinko <glukym@gmail.com>
2019-02-12 16:46:48 +0200

    Message was added

--- js/main.js
+++ js/main.js
@@ -13,3 +13,25 @@ new Voter({
 });
 
 
+
+
+
+// let message1 = new Message(
+//   document.querySelector('[data-component="voter1"]'),
+//   { text: 'Hello 1', title: 'Message 1' }
+// );
+//
+// setInterval(() => {
+//   message1.setProps({ text: Math.random() });
+// }, 1000);
+//
+// setTimeout(() => {
+//   message1.setProps({ title: 'New title' });
+// }, 5000);
+//
+//
+// let message2 = new Message(
+//   document.querySelector('[data-component="voter2"]'),
+// );
+
+
